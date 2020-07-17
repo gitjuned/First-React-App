12bacdc5e38c052a18f6353115941a185b629804
Juned Khatri <junaydk46@gmail.com>
2020-07-17 09:45:28 +0530

    FIX: minor change

--- src/App.js
+++ src/App.js
@@ -1,77 +1,83 @@
-import React, { Component } from 'react'
-import NameData from './components/NameData';
-import AddItem from './components/AddItem';
+import React, { Component } from "react";
+import NameData from "./components/NameData";
+import AddItem from "./components/AddItem";
 
+// Just a minor change
 class App extends Component {
-
   state = {
     nameData: [
       {
         id: 1,
-        fname: 'juned',
-        lname: 'khatri'
+        fname: "juned",
+        lname: "khatri",
       },
       {
         id: 2,
-        fname: 'rahil',
-        lname: 'bhanushali'
+        fname: "rahil",
+        lname: "bhanushali",
       },
       {
         id: 3,
-        fname: 'kanishka',
-        lname: 'khemani'
+        fname: "kanishka",
+        lname: "khemani",
       },
     ],
-    error: false
-  }
+    error: false,
+  };
 
   delElement = (id) => {
-    this.setState({ nameData: [...this.state.nameData.filter(data => data.id !== id)] });
-  }
+    this.setState({
+      nameData: [...this.state.nameData.filter((data) => data.id !== id)],
+    });
+  };
 
   submitHandler = (e, state, ctx) => {
-    e.preventDefault()
+    e.preventDefault();
     console.log("ctx", ctx);
-    console.log("clicked submit this.state", state)
+    console.log("clicked submit this.state", state);
 
-    if ((e.target.elements.fname.value === "") || (e.target.elements.lname.value === "")) {
+    if (
+      e.target.elements.fname.value === "" ||
+      e.target.elements.lname.value === ""
+    ) {
       alert("all fields are compulsory");
-    }
-    else {
+    } else {
       let validation = this.state.nameData.every((element) => {
-
-        if ((element.fname === state.fname.toLowerCase()) && (element.lname === state.lname.toLowerCase())) {
-          return false
-        }
-        else {
-          return true
+        if (
+          element.fname === state.fname.toLowerCase() &&
+          element.lname === state.lname.toLowerCase()
+        ) {
+          return false;
+        } else {
+          return true;
         }
       });
 
       if (validation) {
         this.setState({
-          nameData: [...this.state.nameData, { id: this.state.nameData.length + 1, fname: state.fname, lname: state.lname }],
-          error: false
+          nameData: [
+            ...this.state.nameData,
+            {
+              id: this.state.nameData.length + 1,
+              fname: state.fname,
+              lname: state.lname,
+            },
+          ],
+          error: false,
         });
         ctx.setState({
           fname: "",
-          lname: ""
-        })
-      }
-      else {
-        this.setState({ error: true })
+          lname: "",
+        });
+      } else {
+        this.setState({ error: true });
         ctx.setState({
           fname: "",
-          lname: ""
-        })
+          lname: "",
+        });
       }
-
     }
-
-
   };
-
-
 
   render() {
     return (
@@ -80,17 +86,13 @@ class App extends Component {
           nameData={this.state.nameData}
           submitItem={this.submitHandler}
         />
-        {
-          this.state.error ?
-            <p style={{ color: 'red' }}>First and Last Name is already present</p> : null
-        }
-        <NameData
-          nameData={this.state.nameData}
-          delElement={this.delElement}
-        />
+        {this.state.error ? (
+          <p style={{ color: "red" }}>First and Last Name is already present</p>
+        ) : null}
+        <NameData nameData={this.state.nameData} delElement={this.delElement} />
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
